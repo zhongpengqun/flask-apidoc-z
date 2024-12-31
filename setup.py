07bb7501dd42d436dabc1b2e3f219ab613fd55c7
zhong (钟鹏群) <3602551038@qq.com>
2024-12-31 16:02:43 +0800

    f

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 # coding=utf8
-__author__ = 'liming'
+__author__ = 'Zhong.PengQun'
 
 from setuptools import setup
 
-setup(name='flask_doc',
-      version='0.2.6',
-      description='Write API document when you coding, Test your API when you press last word immediately',
-      url='https://github.com/ipconfiger/flask-apidoc',
-      author='Alexander.Li',
-      author_email='superpowerlee@gmail.com',
-      license='GNU GENERAL PUBLIC LICENSE',
-      packages=['flask_doc'],
+setup(name='flask_doc_z',
+      version='0.0.3',
+      description='Simple Flask API doc generator，forked from https://github.com/ipconfiger/flask-apidoc',
+      url='',
+      author='Zhong.PengQun',
+      author_email='',
+      license='',
+      packages=['flask_doc_z'],
       package_data = {
-            "flask_doc": ["*.html", "*.gif"],
+            "flask_doc_z": ["*.html", "*.gif"],
       },
       install_requires=[
           'flask','markdown'
       ],
       entry_points = {
-        'console_scripts': ['fkapidoc=flask_doc.generator:main'],
+        'console_scripts': ['fkapidoc=flask_doc_z.generator:main'],
       },
       zip_safe=False)
